138bef00f761f6f5907d30c3c7647019a23ebf0a
Shriyakhanna1998 <shriyakhanna1998@gmail.com>
2021-06-25 15:29:56 +0530

    Fix db connectivity.

--- src/utils/dbconnect.js
+++ src/utils/dbconnect.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME,
-	port: process.env.DB_PORT
+	host: process.env.RDS_HOSTNAME,
+	user: process.env.RDS_USERNAME,
+	password: process.env.RDS_PASSWORD,
+	port: process.env.RDS_PORT
 })
-connection.connect()
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
 module.exports = connection
